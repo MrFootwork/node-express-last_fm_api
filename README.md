deef754d4d56da60c8b8e42a55571c5dce0f4b3b
MrFootwork <pandau.ting@gmail.com>
2022-11-03 04:30:33 +0100

    🚧 restructuered file structure

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+[API Key](https://www.last.fm/api/accounts)
 
-https://www.last.fm/api/accounts
+# Client (Vue setup)
 
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- api/server.js
+++ api/server.js
@@ -22,14 +22,17 @@ app.listen(port, () => {
 app.get('/api', async (req, res) => {
 	// read request
 	const queryObject = url.parse(req.url, true).query // { artist: 'cher' }
+
 	// build query
 	const artistToSearchFor = queryObject.artist || ''
 	const apiKey = process.env.API_KEY || 0
 	const lastFm_url = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistToSearchFor}&api_key=${apiKey}&format=json`
+
 	// process response
 	const lastFm_response = await fetch(lastFm_url)
 	const lastFm_data = await lastFm_response.json()
-	const lastFm_artists = lastFm_data.results.artistmatches.artist
+	const lastFm_artists = lastFm_data.results?.artistmatches?.artist
+
 	// shape data structure
 	const lastFm_formatted = lastFm_artists.map(artist => {
 		const imgIndexSmall = 0
